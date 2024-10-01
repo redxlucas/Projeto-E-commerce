8068df4c4e9ae07b6ef61e248d73e25011f61173
Lucas Azevedo <lucassetem@gmail.com>
2024-09-30 21:48:24 -0300

    commit teste

--- src/main/java/br/edu/ifrs/loja/controllers/GeneralController.java
+++ src/main/java/br/edu/ifrs/loja/controllers/GeneralController.java
@@ -1,7 +1,12 @@
 package br.edu.ifrs.loja.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifrs.loja.models.Product;
@@ -9,16 +14,32 @@ import br.edu.ifrs.loja.models.Product;
 @RestController
 public class GeneralController {
     
-    @PostMapping("product")
-    public Product create(Product product){
+    @PostMapping("/produto")
+    public Product createProduct(Product product){
         product.insert();
         return product;
     }
 
-    @DeleteMapping("product")
-    public Boolean delete(Product product){
-        
-        return true;
-        
+    @GetMapping("/produto/{id}")
+    public Product getProduct(@PathVariable int id){
+        Product product = new Product();
+        product.setId(id);
+        product.get();
+        return product;
+    }
+
+    @GetMapping("/produto")
+    public ArrayList<Product> listAll(){
+        return Product.getAll();
+    }
+
+    @PutMapping("/produto")
+    public void updateProduct(Product product){
+        product.update();
+    }
+
+    @DeleteMapping("/produto")
+    public void deleteProduct(Product product){
+        product.delete();
     }
 }
--- src/main/java/br/edu/ifrs/loja/models/Product.java
+++ src/main/java/br/edu/ifrs/loja/models/Product.java
@@ -5,8 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Product{
+//#region
     private int id;
     private String name;
     private double value;
@@ -29,15 +31,15 @@ public class Product{
     public void setValue(double value) {
         this.value = value;
     }
-
+//#endregion
     public void insert(){
         DBConnection c = new DBConnection();
         Connection newConnection = c.getConnection();
 
-        String insertIntoProduct = "INSERT INTO lojateste (name, value) VALUES (?,?)";
+        String insertProduct = "INSERT INTO lojateste (name, value) VALUES (?,?)";
 
         try{
-            PreparedStatement ps = newConnection.prepareStatement(insertIntoProduct, Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement ps = newConnection.prepareStatement(insertProduct, Statement.RETURN_GENERATED_KEYS);
             
             ps.setString(1, this.name);
             ps.setDouble(2, this.value);
@@ -47,9 +49,85 @@ public class Product{
             if(rs.next()){
                 this.id = rs.getInt(1);
             }
-
         } catch (SQLException e){
             e.printStackTrace();
         }
+    }
+
+    public void get(){
+        DBConnection c = new DBConnection();
+        Connection newConnection = c.getConnection();
+
+        String selectProduct = "SELECT * FROM lojateste WHERE id = ?";
+
+        try{
+            PreparedStatement ps = newConnection.prepareStatement(selectProduct);
+            ps.setInt(1, this.id);
+
+            ResultSet resultSet = ps.executeQuery();
+
+            if(resultSet.next()){
+                this.setName(resultSet.getString("name"));
+                this.setValue(resultSet.getDouble("value"));
+            }
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<Product> getAll(){
+        DBConnection c = new DBConnection();
+        Connection newConnection = c.getConnection();
+        ArrayList<Product> products = new ArrayList<>();
+
+        String selectProduct = "SELECT * FROM lojateste";
+
+        try{
+            Statement statement = newConnection.createStatement();
+            ResultSet resultSet = statement.executeQuery(selectProduct);
+            while(resultSet.next()){
+                Product product = new Product();
+                product.setId(resultSet.getInt("id"));
+                product.setName(resultSet.getString("name"));
+                product.setValue(resultSet.getDouble("value"));
+                products.add(product);
+            }
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+        return products;
+    }
+
+    public void update(){
+        DBConnection c = new DBConnection();
+        Connection newConnection = c.getConnection();
+
+        String updateProduct = "UPDATE lojateste SET name = ?, value = ? WHERE id = ?";
+
+        try{
+            PreparedStatement ps = newConnection.prepareStatement(updateProduct);
+            ps.setString(1, this.name);
+            ps.setDouble(2, this.value);
+            ps.setInt(3, this.id);
+            ps.executeUpdate();
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public boolean delete(){
+        DBConnection c = new DBConnection();
+        Connection newConnection = c.getConnection();
+
+        String deleteProduct = "DELETE FROM lojateste WHERE id = ?";
+
+        try{
+            PreparedStatement ps = newConnection.prepareStatement(deleteProduct);
+            ps.setInt(1, this.id);
+            ps.executeUpdate();
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+        return true;
     }
 }
